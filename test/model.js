e3a56b9ce668ea488763454cf19ad188c18dc398
João Paulo Dubas <joao.dubas@gmail.com>
2014-01-09 06:04:02 -0200

    [improve] Add test to validation, model creation and instance creation.

--- test/model.js
+++ test/model.js
@@ -1,4 +1,5 @@
 'use strict';
+/* global describe, it */
 const expect = require('chai').expect;
 const model = require('../lib/model.js');
 
@@ -12,6 +13,10 @@ describe('model', function () {
   ];
 
   describe('#createModel', function () {
+    it('throw error if no model name is provided', function () {
+      expect(function () { model.createModel(); }).to.throw(TypeError);
+    });
+
     it('create an empty model', function () {
       let User = model.createModel(modelName);
       expect(User).to.be.a('function');
@@ -160,6 +165,40 @@ describe('model', function () {
         expect(function () { thrower('a'); }).to.throw(TypeError);
         expect(function () { thrower(1); }).to.not.throw(TypeError);
       });
+
+      describe('types', function () {
+        var properties = [
+          {name: 'pNum', type: 'number', required: false},
+          {name: 'pStr', type: 'string', required: false},
+          {name: 'pDate', type: 'date', required: false},
+          {name: 'pBool', type: 'boolean', required: false}
+        ];
+        var User = model.createModel('User', properties);
+        function thrower(property, value) {
+          var user = new User();
+          user[property] = value;
+        }
+
+        it('ensure number', function () {
+          expect(function () { thrower('pNum', 'a'); }).to.throw(TypeError);
+          expect(function () { thrower('pNum', 1); }).to.not.throw(TypeError);
+        });
+
+        it('ensure string', function () {
+          expect(function () { thrower('pStr', true); }).to.throw(TypeError);
+          expect(function () { thrower('pStr', 'a'); }).to.not.throw(TypeError);
+        });
+
+        it('ensure date', function () {
+          expect(function () { thrower('pDate', true); }).to.throw(TypeError);
+          expect(function () { thrower('pDate', new Date()); }).to.not.throw(TypeError);
+        });
+
+        it('ensure boolean', function () {
+          expect(function () { thrower('pBool', 1); }).to.throw(TypeError);
+          expect(function () { thrower('pBool', true); }).to.not.throw(TypeError);
+        });
+      });
     });
 
     describe('custom validators', function () {
@@ -180,10 +219,10 @@ describe('model', function () {
           user.property = value;
         }
 
-        expect(function () { thrower(null) }).to.throw(TypeError);
-        expect(function () { thrower('a') }).to.throw(TypeError);
-        expect(function () { thrower(11) }).to.throw(TypeError);
-        expect(function () { thrower(10) }).to.not.throw(TypeError);
+        expect(function () { thrower(null); }).to.throw(TypeError);
+        expect(function () { thrower('a'); }).to.throw(TypeError);
+        expect(function () { thrower(11); }).to.throw(TypeError);
+        expect(function () { thrower(10); }).to.not.throw(TypeError);
       });
 
       it('must be an array', function () {
@@ -225,11 +264,11 @@ describe('model', function () {
           user.property = value;
         }
 
-        expect(function () { thrower(null) }).to.throw(TypeError);
-        expect(function () { thrower('a') }).to.throw(TypeError);
-        expect(function () { thrower(9) }).to.throw(TypeError);
-        expect(function () { thrower(20) }).to.throw(TypeError);
-        expect(function () { thrower(11) }).to.not.throw(TypeError);
+        expect(function () { thrower(null); }).to.throw(TypeError);
+        expect(function () { thrower('a'); }).to.throw(TypeError);
+        expect(function () { thrower(9); }).to.throw(TypeError);
+        expect(function () { thrower(20); }).to.throw(TypeError);
+        expect(function () { thrower(11); }).to.not.throw(TypeError);
       });
 
       it('can be used without required', function () {
@@ -248,10 +287,10 @@ describe('model', function () {
           user.property = value;
         }
 
-        expect(function () { thrower(null) }).to.not.throw(TypeError);
-        expect(function () { thrower('a') }).to.throw(TypeError);
-        expect(function () { thrower(11) }).to.throw(TypeError);
-        expect(function () { thrower(10) }).to.not.throw(TypeError);
+        expect(function () { thrower(null); }).to.not.throw(TypeError);
+        expect(function () { thrower('a'); }).to.throw(TypeError);
+        expect(function () { thrower(11); }).to.throw(TypeError);
+        expect(function () { thrower(10); }).to.not.throw(TypeError);
       });
 
       it('can be used without a type', function () {
@@ -270,10 +309,10 @@ describe('model', function () {
           user.property = value;
         }
 
-        expect(function () { thrower(null) }).to.throw(TypeError);
-        expect(function () { thrower('a') }).to.throw(TypeError);
-        expect(function () { thrower(11) }).to.throw(TypeError);
-        expect(function () { thrower(10) }).to.not.throw(TypeError);
+        expect(function () { thrower(null); }).to.throw(TypeError);
+        expect(function () { thrower('a'); }).to.throw(TypeError);
+        expect(function () { thrower(11); }).to.throw(TypeError);
+        expect(function () { thrower(10); }).to.not.throw(TypeError);
       });
 
       it('can be used without required and type', function () {
@@ -291,29 +330,26 @@ describe('model', function () {
           user.property = value;
         }
 
-        expect(function () { thrower(null) }).to.not.throw(TypeError);
-        expect(function () { thrower('a') }).to.throw(TypeError);
-        expect(function () { thrower(11) }).to.throw(TypeError);
-        expect(function () { thrower(10) }).to.not.throw(TypeError);
-      });
-    });
-
-    describe('number', function () {
-      let properties = [
-        { name: 'property', type: 'number', required: false }
-      ];
-      let User = model.createModel(modelName, properties);
-
-      it('validate number field', function () {
-        expect(function () {
-          let user = new User();
-          user.property = 'a';
-        }).to.throw(TypeError);
+        expect(function () { thrower(null); }).to.not.throw(TypeError);
+        expect(function () { thrower('a'); }).to.throw(TypeError);
+        expect(function () { thrower(11); }).to.throw(TypeError);
+        expect(function () { thrower(10); }).to.not.throw(TypeError);
       });
     });
   });
 
   describe('instance', function () {
+    it('create without `new` keyword', function () {
+      let User = model.createModel(modelName, ['name']);
+      function creator() {
+        return User();
+      }
+
+      expect(creator).to.not.throw(Error);
+      expect(creator().constructor.name).to.be.equal(User.name);
+      expect(creator().constructor.model).to.be.equal(User.model);
+    });
+
     it('do not share property values', function () {
       let properties = ['name', 'email', 'password'];
       let User = model.createModel(modelName, properties);

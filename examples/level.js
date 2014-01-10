44c5013ebe47c3d1e40b19e8c0d2c72fe4325494
João Paulo Dubas <joao.dubas@gmail.com>
2014-01-10 17:40:36 -0200

    [improve] Create database reference and add static methods to model.

--- examples/level.js
+++ examples/level.js
@@ -3,6 +3,14 @@
 const levelco = require('level-co');
 const memdown = require('memdown');
 const model = require('../');
+const db = levelco.deferred(
+  'db',
+  {
+    db: function (l) {
+      return new (memdown)(l);
+    }
+  }
+);
 
 var attributes = {
   username: 'me',
@@ -10,5 +18,9 @@ var attributes = {
   password: 'mepass'
 };
 const properties = Object.keys(attributes);
-const User = model.createMolde('User', properties);
-
+const User = model.createModel('User', properties);
+const access = {
+  get: function *() { yield null; },
+  put: function *() { yield null; },
+  del: function *() { yield null; }
+};
